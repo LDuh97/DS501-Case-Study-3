cd0c0bf15aeeefe8a8480d9e1cf8258297a56ac1
LDuh97 <95402924+LDuh97@users.noreply.github.com>
2021-12-02 11:01:22 -0500

    Delete server.R

--- server.R
+++
@@ -1,9 +0,0 @@
-library (tibble)
-library(plotly)
-library(shiny)
-library(dplyr)
-
-server = function(input,output){
-  rowChosen = reactive({which(Scenarios_Charts$Scenario == input$Scenario)})
-  output$Pie_Chart = renderPlotly(get(Scenarios_Charts[rowChosen(), 2]))
-}
